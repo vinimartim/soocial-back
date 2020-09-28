f1076ebfce4998de948063818dd6f1eb9093e69a
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-09-28 14:19:59 -0300

    Gerador e validação de JWT

---
+++ src/main/java/com/example/demo/utils/JwtUtil.java
@@ -0,0 +1,74 @@
+package com.example.demo.utils;
+
+import com.example.demo.entity.Usuario;
+import com.example.demo.exception.RegradeNegocioException;
+import com.example.demo.services.UsuarioService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtil {
+
+    private String SECRET_KEY = "secret";
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        Usuario usuario = usuarioService
+                .findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new RegradeNegocioException("Usuário não encontrado"));
+
+        claims.put("username", usuario.getUsername());
+        claims.put("email", usuario.getEmail());
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    public String createToken(Map<String, Object> claims, String subject) {
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
