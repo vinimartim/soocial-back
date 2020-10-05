af527ac7d8eaffc750908f80a54950b9908fa15b
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-04 21:26:33 -0300

    Adicionando dto para grupo

---
+++ src/main/java/com/example/demo/dto/GrupoDTO.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class GrupoDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 5)
+    private String nome;
+
+    @NotBlank
+    private String descricao;
+
+    @NotBlank
+    private String privacidade;
+
+    @NotBlank
+    private Usuario dono;
+
+    private List<Usuario> administradores;
+    private List<Usuario> membros;
+
+    public GrupoDTO() {
+        this.administradores = new ArrayList<>();
+        this.membros = new ArrayList<>();
+    }
+}
